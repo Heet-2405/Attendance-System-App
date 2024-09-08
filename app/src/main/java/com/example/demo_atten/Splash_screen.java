878c8d7d8e9e19560fc1eff12736f55529e73fb6
heet <chotaliyaheet@gmail.com>
2024-09-09 01:22:49 +0530

    splash screen added

---
+++ app/src/main/java/com/example/demo_atten/Splash_screen.java
@@ -0,0 +1,31 @@
+package com.example.demo_atten;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Finish the splash activity
+            }
+        }, 2000);
+
+    }
+}
